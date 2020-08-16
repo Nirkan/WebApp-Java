5e07d1b27ed276beac7feeffeee1de80339d5256
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-16 14:15:15 +0200

    car class

---
+++ Vechile-spring/Car.java
@@ -0,0 +1,33 @@
+package com.niraj.kandpal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car implements Vechile {
+	
+	@Autowired
+	private Tyre tyre;
+	
+	
+	
+	public Tyre getTyre() {
+		return tyre;
+	}
+
+
+
+	public void setTyre(Tyre tyre) {
+		this.tyre = tyre;
+	}
+
+
+
+	public void drive()
+	{
+	
+		System.out.println("Car " + tyre);
+	
+	}
+
+}
