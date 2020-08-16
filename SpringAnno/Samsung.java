df60ea7b0c9e44b79c191481616073e08425d34b
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-08-16 15:50:43 +0200

    Samsung class

---
+++ SpringAnno/Samsung.java
@@ -0,0 +1,33 @@
+package com.kandpal.SpringAnno;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Samsung {
+	
+	@Autowired
+	@Qualifier("mediaTech")
+	MobileProcessor cpu;
+
+	
+	public MobileProcessor getCpu() {
+		return cpu;
+	}
+
+
+	public void setCpu(MobileProcessor cpu) {
+		this.cpu = cpu;
+	}
+
+
+	
+	public void config() {
+		
+		System.out.println("Octa Core, 4 gb Ram, 12 Mp Camera");
+		cpu.process();
+		
+	}
+
+}
